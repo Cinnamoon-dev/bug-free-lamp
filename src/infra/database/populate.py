d4d3cf4fb134311382f2a01ac8758daaea835ae2
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-06-29 20:38:00 -0300

    style: file formatted

--- src/infra/database/populate.py
+++ src/infra/database/populate.py
@@ -3,6 +3,7 @@ from src.infra.database.database import PgDatabase
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
 def populate() -> None:
     def insert(filePath: str) -> None:
         sql_file = open(BASE_DIR + filePath, "r")
@@ -14,10 +15,11 @@ def populate() -> None:
                 print(line)
                 if line.strip():
                     db.cursor.execute(line)
-            
+
             db.connection.commit()
 
     insert("/scripts/tables.sql")
+
 
 if __name__ == "__main__":
     populate()
