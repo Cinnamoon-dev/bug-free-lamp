a7059e4e4f207e46380e5308fa199d458221ada6
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-06-29 20:35:04 -0300

    fix: default db port changed to int

--- src/infra/database/database.py
+++ src/infra/database/database.py
@@ -34,7 +34,7 @@ class PgDatabase(Database):
     def connect_to_database(self):
         return self.driver.connect(
             host=os.getenv("DB_HOST", "localhost"),
-            port=os.getenv("DB_PORT", "5432"),
+            port=os.getenv("DB_PORT", 5432),
             user=os.getenv("DB_USER", "postgres"),
             password=os.getenv("DB_PASSWORD", "1234"),
             database=os.getenv("DB_NAME", "sqltest"),
