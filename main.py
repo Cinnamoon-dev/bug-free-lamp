19ce20fd3d79a88b4b3026429299780d7e2e6698
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-06-29 20:14:59 -0300

    feat: simple endpoint added

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ from fastapi import FastAPI
 app = FastAPI(debug=True)
 
 
+@app.get("/")
+def main():
+    return {"message": "Hello, World!"}
+
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True)
+    uvicorn.run(
+        "main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True
+    )
