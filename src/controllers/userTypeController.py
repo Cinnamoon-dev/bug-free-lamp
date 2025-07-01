0937d7b2b56027923389ef122cb32febaea0d58b
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-06-30 22:43:19 -0300

    feat: user type controller created with all endpoint

---
+++ src/controllers/userTypeController.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from src.services.userTypeService import UserTypeService
+from src.schemas.userTypeSchema import UserTypeSchema
+
+router = APIRouter(prefix="/user/type", tags=["user type"])
+
+@router.get("/all")
+def user_type_all():
+    data = UserTypeService().get_all()
+    return {"data": data}
+
+@router.get("/view/{user_type_id:int}")
+def user_type_view(user_type_id: int):
+    return
+
+@router.post("/add")
+def user_type_add(user_type: UserTypeSchema):
+    return
+
+@router.put("/edit/{user_type_id:int}")
+def user_type_edit(user_type_id: int):
+    return
+
+@router.delete("/delete/{user_type_id:int}")
+def user_type_delete(user_type_id: int):
+    return
