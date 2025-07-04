874a2fab146c05d3e30e81ba25289cc5f2c36801
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-07-04 20:46:19 -0300

    feat: insert user type service method created

--- src/services/userTypeService.py
+++ src/services/userTypeService.py
@@ -1,4 +1,6 @@
 from typing import Any
+from fastapi.responses import JSONResponse
+from psycopg2.errors import UniqueViolation
 
 from src.infra.database.database import PgDatabase
 from src.schemas.userTypeSchema import UserTypeSchema
@@ -6,8 +8,8 @@ from src.schemas.userTypeSchema import UserTypeSchema
 
 class UserTypeService:
     def __init__(self) -> None:
-        self.table = "tipo_usuario"
-        self.columns = ["id", "nome"]
+        self.table: str = "tipo_usuario"
+        self.columns: list[str] = ["id", "nome"]
 
     def get_all(self) -> list[dict[str, Any]]:
         all_user_types = []
@@ -20,5 +22,19 @@ class UserTypeService:
         
         return all_user_types
 
-    def add(self, user_type: UserTypeSchema):
-        return
+    def add(self, user_type: UserTypeSchema) -> JSONResponse:
+        # TODO
+        # Make this generic
+        # table_name, list_of_columns, tuple_with_fields (in the list_of_columns order)
+
+        try:
+            with PgDatabase() as db:
+                db.cursor.execute(f"INSERT INTO {self.table} (nome) VALUES (%s)", (user_type.nome,))
+                db.connection.commit()
+        except UniqueViolation:
+            return JSONResponse(status_code=400, content={"error": True, "message": f"Tipo de usuário com o nome {user_type.nome} já existe"})
+        except Exception:
+            return JSONResponse(status_code=500, content={"error": True, "message": "Database error"})
+        
+        return JSONResponse(status_code=200, content={"error": False, "message": f"Tipo de usuário {user_type.nome} adicionado com sucesso."})
+        
