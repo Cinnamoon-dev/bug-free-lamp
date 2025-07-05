529d11ae6de98d83ae9f979a4b08c1f1f14e552f
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-07-04 21:01:23 -0300

    refactor: endpoints changed to follow restful rules

--- src/controllers/userTypeController.py
+++ src/controllers/userTypeController.py
@@ -4,24 +4,24 @@ from src.schemas.userTypeSchema import UserTypeSchema
 
 router = APIRouter(prefix="/user/type", tags=["user type"])
 
-@router.get("/all")
+@router.get("/")
 def user_type_all():
     data = UserTypeService().get_all()
     return {"data": data}
 
-@router.get("/view/{user_type_id:int}")
+@router.get("/{user_type_id:int}")
 def user_type_view(user_type_id: int):
     return
 
-@router.post("/add")
+@router.post("/")
 def user_type_add(user_type: UserTypeSchema):
     response = UserTypeService().add(user_type)
     return response
 
-@router.put("/edit/{user_type_id:int}")
+@router.put("/{user_type_id:int}")
 def user_type_edit(user_type_id: int):
     return
 
-@router.delete("/delete/{user_type_id:int}")
+@router.delete("/{user_type_id:int}")
 def user_type_delete(user_type_id: int):
     return
