bbd6b9d5705a008f756a29f1171bf569c511bfc5
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-07-07 22:22:45 -0300

    feat: delete user type endpoint created

--- src/controllers/userTypeController.py
+++ src/controllers/userTypeController.py
@@ -6,7 +6,7 @@ router = APIRouter(prefix="/user/type", tags=["user type"])
 
 @router.get("/")
 def user_type_all():
-    data = UserTypeService().get_all()
+    data = UserTypeService().all()
     return {"error": False, "data": data}
 
 @router.get("/{user_type_id:int}")
@@ -26,4 +26,5 @@ def user_type_edit(user_type_id: int, user_type: UserTypeSchema):
 
 @router.delete("/{user_type_id:int}")
 def user_type_delete(user_type_id: int):
-    return
+    response = UserTypeService().delete(user_type_id)
+    return response
