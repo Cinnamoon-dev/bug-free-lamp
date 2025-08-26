3e66f47ada9b582d8ae0aca43738ec3047cd66fc
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-08-26 17:16:00 -0300

    refactor: raising exceptions with errors responses

--- src/services/userService.py
+++ src/services/userService.py
@@ -1,5 +1,6 @@
+from typing import Any
 from functools import reduce
-from fastapi.responses import JSONResponse
+from fastapi import HTTPException
 from fastapi.datastructures import QueryParams
 
 from src.infra.database.database import PgDatabase
@@ -14,7 +15,7 @@ class UserService:
         self.columns = retrieve_table_columns(self.table)
         self.all_columns = reduce(lambda acc, elem: acc + ", " + str(elem), self.columns)
 
-    def all(self, query_params: QueryParams) -> JSONResponse:
+    def all(self, query_params: QueryParams) -> dict[str, Any]:
         show_fk_id = bool(int(query_params.get("show_fk_id", 1)))
         page = int(query_params.get("page", 1))
         rows_per_page = int(query_params.get("rows_per_page", 10))
@@ -26,18 +27,21 @@ class UserService:
             query = f"SELECT u.id, u.email, u.senha, tu.nome AS tipo_usuario FROM {self.table} AS u INNER JOIN tipo_usuario AS tu ON u.tipo_usuario_id=tu.id"
 
         if sort is not None:
-            sort_column, sort_order = sort.split(",")
+            try:
+                sort_column, sort_order = sort.split(",")
+            except Exception:
+                raise HTTPException(status_code=422, detail={"error": True, "message": f"Coluna e direção {sort} não separada corretamente por ','"})
 
             if sort_column not in self.columns:
-                return JSONResponse({"error": True, "message": f"Coluna {sort_column} não identificada"}, 400)
+                raise HTTPException(status_code=422, detail={"error": True, "message": f"Coluna {sort_column} não identificada"})
 
             if sort_order.lower() not in ["asc", "desc"]:
-                return JSONResponse({"error": True, "message": f"Direção de ordenação {sort_order} inválida, deve ser 'asc' ou 'desc'"}, 400)
+                raise HTTPException(status_code=422, detail={"error": True, "message": f"Direção de ordenação {sort_order} inválida, deve ser 'asc' ou 'desc'"})
         
         output = paginate(query, page, rows_per_page, sort) 
-        return JSONResponse(output, 200)
+        return output
 
-    def view(self, user_id: int) -> JSONResponse:
+    def view(self, user_id: int) -> dict[str, Any]:
         user = None
 
         try:
@@ -46,33 +50,33 @@ class UserService:
                 row = db.cursor.fetchone()
 
                 if row is None:
-                    return JSONResponse(status_code=404, content={"error": True, "message": "Usuário não encontrado"})
+                    raise HTTPException(status_code=404, detail={"error": True, "message": "Usuário não encontrado"})
         except Exception:
-            return JSONResponse(status_code=500, content={"error": True, "message": "Database error"})
+            raise HTTPException(status_code=500, detail={"error": True, "message": "Database error"})
         
         user = line_to_dict(row, self.columns)
-        return JSONResponse(status_code=200, content={"error": False, "data": user})
+        return user
     
-    def add(self, user: UserAddSchema) -> JSONResponse:
+    def add(self, user: UserAddSchema) -> dict[str, Any]:
         try:
             with PgDatabase() as db:
                 db.cursor.execute(f"INSERT INTO {self.table} (email, senha, tipo_usuario_id) VALUES (%s, %s, %s) RETURNING id", (user.email, user.senha, user.tipo_usuario_id))
                 raw_id = db.cursor.fetchone()
 
                 if raw_id is None:
-                    return JSONResponse(status_code=500, content={"error": True, "message": f"Não foi possível inserir o usuário {user.email}."})
+                    raise HTTPException(status_code=500, detail={"error": True, "message": f"Não foi possível inserir o usuário {user.email}."})
 
                 inserted_id = raw_id[0]
                 db.connection.commit()
         except Exception as e:
-            return JSONResponse(status_code=500, content={"error": True, "message": str(e)})
+            raise HTTPException(status_code=500, detail={"error": True, "message": str(e)})
         
-        return JSONResponse(status_code=200, content={"error": False, "message": f"Usuário {user.email} adicionado com sucesso.", "id": inserted_id})
+        return {"error": False, "message": f"Usuário {user.email} adicionado com sucesso.", "id": inserted_id}
 
-    def edit(self, user_id: int, user: UserEditSchema) -> JSONResponse:
+    def edit(self, user_id: int, user: UserEditSchema) -> dict[str, Any]:
         user_dict = user.model_dump(exclude_none=True)
         if not user_dict:
-            return JSONResponse(status_code=200, content={"error": False, "message": f"Usuário com id {user_id} editado com sucesso."})
+            raise HTTPException(status_code=200, detail={"error": False, "message": f"Usuário com id {user_id} editado com sucesso."})
 
         set_fields, set_values = fields_to_update(user_dict)
         try:
@@ -80,16 +84,16 @@ class UserService:
                 db.cursor.execute(f"UPDATE {self.table} SET {set_fields} WHERE id = %s", set_values + (user_id,))
                 db.connection.commit()
         except Exception:
-            return JSONResponse(status_code=500, content={"error": True, "message": "Database error"})
+            raise HTTPException(status_code=500, detail={"error": True, "message": "Database error"})
 
-        return JSONResponse(status_code=200, content={"error": False, "message": f"Usuário com id {user_id} editado com sucesso."})
+        return {"error": False, "message": f"Usuário com id {user_id} editado com sucesso."}
 
-    def delete(self, user_id: int) -> JSONResponse:
+    def delete(self, user_id: int) -> dict[str, Any]:
         try:
             with PgDatabase() as db:
                 db.cursor.execute(f"DELETE FROM {self.table} WHERE id = %s", (user_id,))
                 db.connection.commit()
         except Exception:
-            return JSONResponse(status_code=500, content={"error": True, "message": "Database error"})
+            raise HTTPException(status_code=500, detail={"error": True, "message": "Database error"})
 
-        return JSONResponse(status_code=200, content={"error": False, "message": f"Usuário com id {user_id} deletado com sucesso."})
+        return {"error": False, "message": f"Usuário com id {user_id} deletado com sucesso."}
