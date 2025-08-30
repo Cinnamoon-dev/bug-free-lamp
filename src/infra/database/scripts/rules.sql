941dc443fa7d86eb99a3f0837324bfbd30752d83
Pedro Leandro <pedrolcsilva10@gmail.com>
2025-08-29 22:13:41 -0300

    feat: base rules created

---
+++ src/infra/database/scripts/rules.sql
@@ -0,0 +1,21 @@
+-- Tipo Usuário
+INSERT INTO tipo_usuario (id, nome) VALUES (1, "admin");
+
+-- Controllers
+INSERT INTO controllers (id, nome) VALUES (1, "usuario");
+INSERT INTO controllers (id, nome) VALUES (2, "tipo_usuario");
+
+-- Rules
+-- admin + usuario controller
+INSERT INTO regras (id, controller_id, tipo_usuario_id, acao, permitir) VALUES (1, 1, 1, "all", True);
+INSERT INTO regras (id, controller_id, tipo_usuario_id, acao, permitir) VALUES (2, 1, 1, "view", True);
+INSERT INTO regras (id, controller_id, tipo_usuario_id, acao, permitir) VALUES (3, 1, 1, "add", True);
+INSERT INTO regras (id, controller_id, tipo_usuario_id, acao, permitir) VALUES (4, 1, 1, "edit", True);
+INSERT INTO regras (id, controller_id, tipo_usuario_id, acao, permitir) VALUES (5, 1, 1, "delete", True);
+
+-- admin + tipo_usuario controller
+INSERT INTO regras (id, controller_id, tipo_usuario_id, acao, permitir) VALUES (6, 2, 1, "all", True);
+INSERT INTO regras (id, controller_id, tipo_usuario_id, acao, permitir) VALUES (7, 2, 1, "view", True);
+INSERT INTO regras (id, controller_id, tipo_usuario_id, acao, permitir) VALUES (8, 2, 1, "add", True);
+INSERT INTO regras (id, controller_id, tipo_usuario_id, acao, permitir) VALUES (9, 2, 1, "edit", True);
+INSERT INTO regras (id, controller_id, tipo_usuario_id, acao, permitir) VALUES (10, 2, 1, "delete", True);
