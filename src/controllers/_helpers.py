559aa39cf0d703747fa3ab883fbb7c582923df76
Pedro Leandro <pedrolcsilva10@gmail.com>
2025-08-29 23:40:34 -0300

    feat: permission checker created

--- src/controllers/_helpers.py
+++ src/controllers/_helpers.py
@@ -1,9 +1,9 @@
-import jwt
 from typing import Annotated, Any
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from src.services.userService import UserService
+from src.infra.database.database import PgDatabase
 from src.infra.security.hashing import ALGORITHM, JWT_ACCESS_SECRET_KEY, decode_token
 
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/login")
@@ -23,3 +23,26 @@ def get_current_user(token: token_dependency) -> dict[str, Any]:
 
 user_dependency = Annotated[dict[str, Any], Depends(get_current_user)]
 form_auth_dependency = Annotated[OAuth2PasswordRequestForm, Depends()]
+
+class PermissionChecker():
+    def __init__(self, required_permission: str):
+        self.required_permission = required_permission
+
+    def __call__(self, user: user_dependency):
+        controller, action = self.required_permission.split("-")
+        user_type_id = user["tipo_usuario_id"]
+
+        query = """
+            SELECT regras.acao, regras.permitir, controllers.nome, tipo_usuario.nome 
+            FROM regras
+            JOIN controllers ON controllers.id = regras.controller_id
+            JOIN tipo_usuario ON tipo_usuario.id = regras.tipo_usuario_id
+            WHERE tipo_usuario.id = %s AND regras.permitir = True AND controllers.nome = %s AND regras.acao = %s
+        """
+
+        with PgDatabase() as db:
+            db.cursor.execute(query, (user_type_id, controller, action))
+            row = db.cursor.fetchone()
+
+            if row is None:
+                raise HTTPException(status_code=403, detail={"message": "Usuário não autorizado", "error": True})
